0ced08c2332b6e3af5bc31a12568840e9160bb6a
Elliott Slaughter <eds@google.com>
2010-06-30 11:40:42 -0700

    Browser save page as web archive.
    Change-Id: I01366f575f61fd6e2084ad003b860f5c6083258e

--- src/com/android/browser/BrowserActivity.java
+++ src/com/android/browser/BrowserActivity.java
@@ -1403,6 +1403,23 @@ public class BrowserActivity extends Activity
                 showFindDialog();
                 break;
 
+            case R.id.save_webarchive_menu_id:
+                if (LOGD_ENABLED) {
+                    Log.d(LOGTAG, "Save as Web Archive");
+                }
+                String directory = getExternalFilesDir(null).getAbsolutePath() + File.separator;
+                getTopWindow().saveWebArchive(directory, true, new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+                        if (value != null) {
+                            Toast.makeText(BrowserActivity.this, R.string.webarchive_saved, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(BrowserActivity.this, R.string.webarchive_failed, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                break;
+
             case R.id.page_info_menu_id:
                 showPageInfo(mTabControl.getCurrentTab(), false);
                 break;
