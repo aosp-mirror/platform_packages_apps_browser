c5998c2068933250cbacb9c904b45b8608835d18
Michael Kolb <kolby@google.com>
2010-10-10 16:04:35 -0700

    fix dropdown width issue
        http://b/issue?id=3041817
    
    Change-Id: I9bd7de3a2f0d742739a1b8bf2011314ebc617eca

--- src/com/android/browser/UrlInputView.java
+++ src/com/android/browser/UrlInputView.java
@@ -88,11 +88,16 @@ public class UrlInputView extends AutoCompleteTextView
     @Override
     public void showDropDown() {
         int width = mContainer.getWidth();
-        if ((mAdapter.getLeftCount() == 0) || (mAdapter.getRightCount() == 0)) {
+        if (mLandscape && ((mAdapter.getLeftCount() == 0) ||
+                (mAdapter.getRightCount() == 0))) {
             width = width / 2;
         }
-        setDropDownWidth(width);
-        setDropDownHorizontalOffset(-getLeft());
+        if (width != getDropDownWidth()) {
+            setDropDownWidth(width);
+        }
+        if (getLeft() != -getDropDownHorizontalOffset()) {
+            setDropDownHorizontalOffset(-getLeft());
+        }
         mAdapter.setLandscapeMode(mLandscape);
         super.showDropDown();
     }
