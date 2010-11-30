a4909cba214f3680063367e52044e9d74824c272
Ben Murdoch <benm@google.com>
2010-11-30 20:48:15 +0000

    Hide the custom view when we get onPause() DO NOT MERGE
    When we run onPause, be sure to hide any custom views (such as
    the HTML5 video view) so that when the activity restores we don't
    have anything overlaying the page.
    
    Bug: 3240426
    Change-Id: Ic93286eab40cda814682bc023f3648b502545256

--- src/com/android/browser/BrowserActivity.java
+++ src/com/android/browser/BrowserActivity.java
@@ -942,6 +942,10 @@ public class BrowserActivity extends Activity
         // unregister network state listener
         unregisterReceiver(mNetworkStateIntentReceiver);
         WebView.disablePlatformNotifications();
+
+        if (mCustomView != null) {
+            mTabControl.getCurrentWebView().getWebChromeClient().onHideCustomView();
+        }
     }
 
     @Override
@@ -2768,7 +2772,6 @@ public class BrowserActivity extends Activity
     void onHideCustomView() {
         if (mCustomView == null)
             return;
-
         // Hide the custom view.
         mCustomView.setVisibility(View.GONE);
         // Remove the custom view from its container.
