63bb687da46c285f6a71ff47c9f00e46111ffec2
John Reck <jreck@google.com>
2010-12-01 19:29:32 -0800

    Debug settings changes
     Bug: 3250498
     Makes OpenGL rendering and user agent normally visible in settings->debug
     Changing the OpenGL setting now automatically restarts the browser
    
    Change-Id: I04036b580f2463f77376edb4bee2dfefe3d123ed

--- src/com/android/browser/BrowserPreferencesPage.java
+++ src/com/android/browser/BrowserPreferencesPage.java
@@ -34,7 +34,7 @@ public class BrowserPreferencesPage extends PreferenceActivity {
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.preference_headers, target);
 
-        if (BrowserSettings.getInstance().showDebugSettings()) {
+        if (BrowserSettings.DEV_BUILD || BrowserSettings.getInstance().showDebugSettings()) {
             Header debug = new Header();
             debug.title = getText(R.string.pref_development_title);
             debug.fragment = DebugPreferencesFragment.class.getName();
