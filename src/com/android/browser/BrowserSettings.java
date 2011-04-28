7fd1e8f86ead29d4cfad3d16e54f4427ac7bf39f
John Reck <jreck@google.com>
2011-04-27 18:22:57 -0700

    Add Honeycomb-MR1 useragent
     Bug: 4347787
    
    Change-Id: Id87528344fecd367381cd546d831eb35f3218f26

--- src/com/android/browser/BrowserSettings.java
+++ src/com/android/browser/BrowserSettings.java
@@ -53,9 +53,9 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
         PreferenceKeys {
 
     // TODO: Do something with this UserAgent stuff
-    private static final String DESKTOP_USERAGENT = "Mozilla/5.0 (Macintosh; " +
-    "U; Intel Mac OS X 10_6_3; en-us) AppleWebKit/533.16 (KHTML, " +
-    "like Gecko) Version/5.0 Safari/533.16";
+    private static final String DESKTOP_USERAGENT = "Mozilla/5.0 (X11; " +
+        "Linux x86_64) AppleWebKit/534.24 (KHTML, like Gecko) " +
+        "Chrome/11.0.696.34 Safari/534.24";
 
     private static final String IPHONE_USERAGENT = "Mozilla/5.0 (iPhone; U; " +
         "CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 " +
@@ -69,11 +69,16 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
         "Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 " +
         "(KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
 
+    private static final String HONEYCOMB_USERAGENT = "Mozilla/5.0 (Linux; U; " +
+        "Android 3.1; en-us; Xoom Build/HMJ25) AppleWebKit/534.13 " +
+        "(KHTML, like Gecko) Version/4.0 Safari/534.13";
+
     private static final String USER_AGENTS[] = { null,
             DESKTOP_USERAGENT,
             IPHONE_USERAGENT,
             IPAD_USERAGENT,
-            FROYO_USERAGENT
+            FROYO_USERAGENT,
+            HONEYCOMB_USERAGENT,
     };
 
     private static BrowserSettings sInstance;
