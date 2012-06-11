51aa5364d5dde094d5703196ba24eb14a8ebbbfd
Andreas Sandblad <andreas.sandblad@sonymobile.com>
2012-06-11 11:02:49 +0200

    Pass webview's useragent when downloading via context menu
    Currently null is passed as useragent to the Download Manager when
    downloading via Browser context menu (causes the Download
    Provider to use an empty useragent HTTP header).
    This commit simply passes the webview's useragent instead.
    
    This applies to the usecase when choosing save image/link
    via the context menu (long press).
    
    Change-Id: Ie9cc5e81630d0dcefe4708980146cc5ed867e4ad

--- src/com/android/browser/Controller.java
+++ src/com/android/browser/Controller.java
@@ -512,8 +512,8 @@ public class Controller
                             case R.id.save_link_context_menu_id:
                             case R.id.download_context_menu_id:
                                 DownloadHandler.onDownloadStartNoStream(
-                                        mActivity, url, null, null, null,
-                                        view.isPrivateBrowsingEnabled());
+                                        mActivity, url, view.getSettings().getUserAgentString(),
+                                        null, null, view.isPrivateBrowsingEnabled());
                                 break;
                         }
                         break;
@@ -1434,8 +1434,9 @@ public class Controller
                     }
                 });
                 menu.findItem(R.id.download_context_menu_id).
-                        setOnMenuItemClickListener(
-                                new Download(mActivity, extra, webview.isPrivateBrowsingEnabled()));
+                setOnMenuItemClickListener(
+                        new Download(mActivity, extra, webview.isPrivateBrowsingEnabled(),
+                                webview.getSettings().getUserAgentString()));
                 menu.findItem(R.id.set_wallpaper_context_menu_id).
                         setOnMenuItemClickListener(new WallpaperHandler(mActivity,
                                 extra));
@@ -2165,6 +2166,7 @@ public class Controller
         private Activity mActivity;
         private String mText;
         private boolean mPrivateBrowsing;
+        private String mUserAgent;
         private static final String FALLBACK_EXTENSION = "dat";
         private static final String IMAGE_BASE_FORMAT = "yyyy-MM-dd-HH-mm-ss-";
 
@@ -2173,16 +2175,18 @@ public class Controller
             if (DataUri.isDataUri(mText)) {
                 saveDataUri();
             } else {
-                DownloadHandler.onDownloadStartNoStream(mActivity, mText, null,
+                DownloadHandler.onDownloadStartNoStream(mActivity, mText, mUserAgent,
                         null, null, mPrivateBrowsing);
             }
             return true;
         }
 
-        public Download(Activity activity, String toDownload, boolean privateBrowsing) {
+        public Download(Activity activity, String toDownload, boolean privateBrowsing,
+                String userAgent) {
             mActivity = activity;
             mText = toDownload;
             mPrivateBrowsing = privateBrowsing;
+            mUserAgent = userAgent;
         }
 
         /**
