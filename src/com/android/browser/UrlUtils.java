3338525823c59867e173c2c65cfed81b6db5c166
Selim Gurun <sgurun@google.com>
2013-07-09 10:41:34 -0700

    Fix the smart search pattern.
    Bug: 9592355
    Change-Id: I1e3ff3f9f0a2038ea5d460e698dceaaa8be50707

--- src/com/android/browser/UrlUtils.java
+++ src/com/android/browser/UrlUtils.java
@@ -33,6 +33,7 @@ public class UrlUtils {
             "(" +    // begin group for schema
             "(?:http|https|file):\\/\\/" +
             "|(?:inline|data|about|javascript):" +
+            "|(?:.*:.*@)" +
             ")" +
             "(.*)" );
 
